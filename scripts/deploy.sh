d0f52b80fc01d665b70dc0c167d325e2d244c015
Simon <simontagger@gmail.com>
2022-08-22 14:37:40 +0200

    deploy fix

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,9 +2,6 @@
 if [ -z "$(sudo -u ubuntu kubectl get deployment | grep deployment-backend)" ]; then
     sudo -u ubuntu kubectl create -f manifest/deployment-backend.yaml
 else
-    echo "docker_tag: ${docker_tag}"
-    [[ -z "${docker_tag}" ]] && exit 1
-
     sudo -u ubuntu kubectl set image deployments/deployment-backend go_backend=jedie20/go_backend:${docker_tag}
 fi
 [[ "$?" != "0" ]] && exit 1
@@ -17,9 +14,6 @@ sudo -u ubuntu kubectl apply -f manifest/service-backend.yaml
 if [ -z "$(sudo -u ubuntu kubectl get deployment | grep deployment-frontend)" ]; then
     sudo -u ubuntu kubectl create -f manifest/deployment-frontend.yaml
 else
-    echo "docker_tag: ${docker_tag}"
-    [[ -z "${docker_tag}" ]] && exit 1
-
     sudo -u ubuntu kubectl image deployments/deployment-frontend go_backend=jedie20/frontend:${docker_tag}
 fi
 [[ "$?" != "0" ]] && exit 1
